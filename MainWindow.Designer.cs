716bc5840708eef8fe3dbd90940f2493247398ab
Jordan <jordankmuguluma@gmail.com>
2021-12-26 21:47:53 -0500

    Fuck Me Chief. Just...wow Changed the ui, Finding Install Locations Works :|

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -32,75 +32,72 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.Version_TextBox = new System.Windows.Forms.TextBox();
             this.Northstar_Version_label = new System.Windows.Forms.Label();
-            this.Install_NS_Button = new System.Windows.Forms.Button();
             this.Install_Textbox = new System.Windows.Forms.TextBox();
             this.Browse_New_Install = new System.Windows.Forms.Button();
             this.Install_Location_Label = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.panel4 = new System.Windows.Forms.Panel();
             this.Log_Label = new System.Windows.Forms.Label();
             this.Log_Box = new System.Windows.Forms.RichTextBox();
             this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.InstallNorthsatar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.InstallNorthsatar);
             this.panel1.Controls.Add(this.Version_TextBox);
             this.panel1.Controls.Add(this.Northstar_Version_label);
-            this.panel1.Controls.Add(this.Install_NS_Button);
             this.panel1.Controls.Add(this.Install_Textbox);
             this.panel1.Controls.Add(this.Browse_New_Install);
             this.panel1.Controls.Add(this.Install_Location_Label);
             this.panel1.Location = new System.Drawing.Point(12, 27);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(348, 134);
+            this.panel1.Size = new System.Drawing.Size(428, 147);
             this.panel1.TabIndex = 0;
             // 
             // Version_TextBox
             // 
+            this.Version_TextBox.BackColor = System.Drawing.SystemColors.Control;
             this.Version_TextBox.Enabled = false;
-            this.Version_TextBox.Location = new System.Drawing.Point(117, 97);
+            this.Version_TextBox.Location = new System.Drawing.Point(9, 73);
             this.Version_TextBox.Name = "Version_TextBox";
             this.Version_TextBox.ReadOnly = true;
-            this.Version_TextBox.Size = new System.Drawing.Size(100, 23);
+            this.Version_TextBox.Size = new System.Drawing.Size(312, 23);
             this.Version_TextBox.TabIndex = 5;
             // 
             // Northstar_Version_label
             // 
             this.Northstar_Version_label.AutoSize = true;
-            this.Northstar_Version_label.Location = new System.Drawing.Point(119, 79);
+            this.Northstar_Version_label.Location = new System.Drawing.Point(133, 55);
             this.Northstar_Version_label.Name = "Northstar_Version_label";
             this.Northstar_Version_label.Size = new System.Drawing.Size(98, 15);
             this.Northstar_Version_label.TabIndex = 4;
             this.Northstar_Version_label.Text = "Northstar Version";
             // 
-            // Install_NS_Button
-            // 
-            this.Install_NS_Button.Location = new System.Drawing.Point(3, 53);
-            this.Install_NS_Button.Name = "Install_NS_Button";
-            this.Install_NS_Button.Size = new System.Drawing.Size(342, 23);
-            this.Install_NS_Button.TabIndex = 3;
-            this.Install_NS_Button.Text = "Install Northstar Launcher";
-            this.Install_NS_Button.UseVisualStyleBackColor = true;
-            // 
             // Install_Textbox
             // 
-            this.Install_Textbox.Location = new System.Drawing.Point(3, 24);
+            this.Install_Textbox.Location = new System.Drawing.Point(9, 24);
             this.Install_Textbox.Name = "Install_Textbox";
             this.Install_Textbox.Size = new System.Drawing.Size(312, 23);
             this.Install_Textbox.TabIndex = 2;
             // 
             // Browse_New_Install
             // 
-            this.Browse_New_Install.Location = new System.Drawing.Point(321, 22);
+            this.Browse_New_Install.Location = new System.Drawing.Point(327, 24);
             this.Browse_New_Install.Name = "Browse_New_Install";
-            this.Browse_New_Install.Size = new System.Drawing.Size(24, 25);
+            this.Browse_New_Install.Size = new System.Drawing.Size(96, 23);
             this.Browse_New_Install.TabIndex = 1;
-            this.Browse_New_Install.Text = "...";
+            this.Browse_New_Install.Text = "Browse";
             this.Browse_New_Install.UseVisualStyleBackColor = true;
             this.Browse_New_Install.Click += new System.EventHandler(this.Browse_New_Install_Click);
             // 
@@ -110,37 +107,48 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Install_Location_Label.AutoSize = true;
-            this.Install_Location_Label.Location = new System.Drawing.Point(43, 6);
+            this.Install_Location_Label.Location = new System.Drawing.Point(114, 6);
             this.Install_Location_Label.Name = "Install_Location_Label";
-            this.Install_Location_Label.Size = new System.Drawing.Size(272, 15);
+            this.Install_Location_Label.Size = new System.Drawing.Size(141, 15);
             this.Install_Location_Label.TabIndex = 0;
-            this.Install_Location_Label.Text = "Titanfall 2 with Northstar Launcher Install Location";
+            this.Install_Location_Label.Text = "Titanfall 2 Install Location";
             this.Install_Location_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Location = new System.Drawing.Point(12, 167);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.checkedListBox1);
+            this.panel2.Location = new System.Drawing.Point(446, 27);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(348, 346);
+            this.panel2.Size = new System.Drawing.Size(448, 462);
             this.panel2.TabIndex = 1;
             // 
-            // panel3
+            // button1
             // 
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Location = new System.Drawing.Point(12, 519);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(692, 58);
-            this.panel3.TabIndex = 2;
+            this.button1.Location = new System.Drawing.Point(3, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(440, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 22);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(440, 346);
+            this.checkedListBox1.TabIndex = 0;
             // 
             // panel4
             // 
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel4.Controls.Add(this.Log_Label);
             this.panel4.Controls.Add(this.Log_Box);
-            this.panel4.Location = new System.Drawing.Point(366, 27);
+            this.panel4.Location = new System.Drawing.Point(12, 288);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(338, 486);
+            this.panel4.Size = new System.Drawing.Size(428, 201);
             this.panel4.TabIndex = 3;
             // 
             // Log_Label
@@ -149,7 +157,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Log_Label.AutoSize = true;
-            this.Log_Label.Location = new System.Drawing.Point(158, 5);
+            this.Log_Label.Location = new System.Drawing.Point(194, 4);
             this.Log_Label.Name = "Log_Label";
             this.Log_Label.Size = new System.Drawing.Size(27, 15);
             this.Log_Label.TabIndex = 0;
@@ -161,10 +169,10 @@
             this.Log_Box.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.Log_Box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Log_Box.Enabled = false;
-            this.Log_Box.Location = new System.Drawing.Point(0, 23);
+            this.Log_Box.Location = new System.Drawing.Point(3, 22);
             this.Log_Box.Name = "Log_Box";
             this.Log_Box.ReadOnly = true;
-            this.Log_Box.Size = new System.Drawing.Size(336, 462);
+            this.Log_Box.Size = new System.Drawing.Size(420, 174);
             this.Log_Box.TabIndex = 0;
             this.Log_Box.Text = "";
             this.Log_Box.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -174,19 +182,45 @@
             this.MainMenuStrip.BackColor = System.Drawing.SystemColors.ControlDark;
             this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MainMenuStrip.Name = "MainMenuStrip";
-            this.MainMenuStrip.Size = new System.Drawing.Size(716, 24);
+            this.MainMenuStrip.Size = new System.Drawing.Size(906, 24);
             this.MainMenuStrip.TabIndex = 4;
             this.MainMenuStrip.Text = "menuStrip1";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button2);
+            this.panel5.Location = new System.Drawing.Point(12, 180);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(428, 102);
+            this.panel5.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(342, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Download Mods From Repo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // InstallNorthsatar
+            // 
+            this.InstallNorthsatar.Location = new System.Drawing.Point(9, 102);
+            this.InstallNorthsatar.Name = "InstallNorthsatar";
+            this.InstallNorthsatar.Size = new System.Drawing.Size(414, 40);
+            this.InstallNorthsatar.TabIndex = 7;
+            this.InstallNorthsatar.Text = "Install Northstar";
+            this.InstallNorthsatar.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(716, 712);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel3);
+            this.ClientSize = new System.Drawing.Size(906, 499);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.MainMenuStrip);
             this.MaximizeBox = false;
@@ -194,8 +228,10 @@
             this.Text = "NorthStar Mod Launcher Version 1.0.0";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,15 +242,18 @@
         private Panel panel1;
         private Label Install_Location_Label;
         private Panel panel2;
-        private Panel panel3;
         private Panel panel4;
         private RichTextBox Log_Box;
         private TextBox Version_TextBox;
         private Label Northstar_Version_label;
-        private Button Install_NS_Button;
         private TextBox Install_Textbox;
         private Button Browse_New_Install;
         private Label Log_Label;
         private MenuStrip MainMenuStrip;
+        private Panel panel5;
+        private Button button1;
+        private CheckedListBox checkedListBox1;
+        private Button button2;
+        private Button InstallNorthsatar;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -6,6 +6,9 @@ namespace Northstar_Manger
 
         public bool Found_Install = false;
         public bool Found_Install_Folder = false;
+        public string Current_Install_Folder = "";
+        private string NSExe, Gamever;
+        private bool NS_Installed;
         public MainWindow()
         {
             InitializeComponent();
@@ -16,205 +19,214 @@ namespace Northstar_Manger
         {
 
         }
-        private string LookForTitanfallInstall()
+        private string Get_And_Set_Filepaths(string rootDir, string Filename)
         {
-            while (true)
+            try
             {
-                try
+                DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(@rootDir);
+                FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + Filename + "*.*");
+                Console.WriteLine(rootDir);
+                Console.WriteLine(Filename);
+
+                foreach (FileInfo foundFile in filesInDir)
                 {
-                    string folderName = "Titanfall2-master";
-                    DriveInfo[] allDrives = DriveInfo.GetDrives();
-                    FileInfo[] files = null;
-                    foreach (DriveInfo dir in allDrives)
+                    if (foundFile.Name.Equals(Filename))
                     {
-                        if (dir.IsReady)
-                        {
-                            DirectoryInfo dirInfo = new DirectoryInfo(dir.Name);
-                            files = dirInfo.GetFiles(folderName + "*.*", SearchOption.AllDirectories);
-                        }
+                        Console.WriteLine("Found");
+
+                        string fullName = foundFile.FullName;
+                        Console.WriteLine(fullName);
+                        return fullName;
+
+
+
                     }
-                    List<string> fileNames = new List<string>();
-                    foreach (FileInfo file in files)
+                    else
                     {
-                        fileNames.Add(file.Name);
+
+                        return "\nCould Not Find"+ Filename+"\n";
+
                     }
 
-                    foreach (object o in fileNames)
-                    {
-                        Log_Box.AppendText(o.ToString());
-                    }
-                }
-                catch (UnauthorizedAccessException e)
-                {
-                    //Log_Box.AppendText("\nCould Not Get"+ e.Message);
-                    continue;
                 }
             }
-            return "\nNo Install Found!";
-
-        }
-        private void FindNSInstall(string Search)
-        {
-
-            // Start with drives if you have to search the entire computer.
-            //string[] drives = System.Environment.GetLogicalDrives();
-
-            //foreach (string dr in drives)
-            //{
-            //    System.IO.DriveInfo di = new System.IO.DriveInfo(dr);
-            //    if (!di.IsReady)
-            //    {
-            //        Console.WriteLine("The drive {0} could not be read", di.Name);
-            //        continue;
-            //    }
-
-            // System.IO.DirectoryInfo rootDir = di.RootDirectory;
-
-            // }
-              //  while (Found_Install_Folder == false)
-            
-                System.IO.DirectoryInfo rootDirs = new DirectoryInfo(@"C:\Program Files");
-
-                WalkDirectoryTree(rootDirs, Search);
-
-                Console.WriteLine("Files with restricted access:");
-                foreach (string s in log)
-                {
-                    Console.WriteLine(s);
-                }
-            
-           
-          
-        }
-        public  string FindGitPath(string firstFilter, string secondFilter, string initialPath)
-        {
-            string gitPath = string.Empty;
-            foreach (var i in Directory.GetDirectories(initialPath))
+            catch (Exception e)
             {
-                try
-                {
-                    foreach (var f in Directory.GetDirectories(i, firstFilter, SearchOption.AllDirectories))
-                    {
-                        if (f != "" || f != null || f.Contains(firstFilter))
-                        {
-                            Log_Box.AppendText("\n"+f);
-                            Found_Install = true;
-
-                        }else
-                        {
-                           Log_Box.AppendText("\nNo Install Found!");
-
-                        }
-                        //foreach (var s in Directory.GetDirectories(f))
-                        //{
-                        //    if (s == Path.Combine(f, secondFilter))
-                        //    {
-                        //        gitPath = f;
-                        //        Log_Box.AppendText(gitPath);
-                        //        break;
-                        //    }
-                        //}
-                    }
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    Log_Box.AppendText("Path is not accessible: {0}"+ i);
-                }
+                Log_Box.AppendText("The process failed: "+ e.ToString());
             }
-            return gitPath;
+            return "Exited with No return";
+
+
+        }
+        private void FindNSInstall(string Search, string FolderDir)
+        {
+
+
+
+            System.IO.DirectoryInfo rootDirs = new DirectoryInfo(@FolderDir);
+
+            WalkDirectoryTree(rootDirs, Search);
+
+            Console.WriteLine("Files with restricted access:");
+            foreach (string s in log)
+            {
+                Console.WriteLine(s);
+            }
+
+
+
         }
         private void contextMenuStrip1_Opening(object sesnder, System.ComponentModel.CancelEventArgs e)
         {
 
         }
-         void WalkDirectoryTree(System.IO.DirectoryInfo root , String Search )
-    {
-        System.IO.FileInfo[] files = null;
-        System.IO.DirectoryInfo[] subDirs = null;
-
-        // First, process all the files directly under this folder
-        try
+        void WalkDirectoryTree(System.IO.DirectoryInfo root, String Search)
         {
-            files = root.GetFiles("*.*");
-        }
-        // This is thrown if even one of the files requires permissions greater
-        // than the application provides.
-        catch (UnauthorizedAccessException e)
-        {
-            // This code just writes out the message and continues to recurse.
-            // You may decide to do something different here. For example, you
-            // can try to elevate your privileges and access the file again.
-            log.Add(e.Message);
-        }
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
 
-        catch (System.IO.DirectoryNotFoundException e)
-        {
-            Console.WriteLine(e.Message);
-        }
+            // First, process all the files directly under this folder
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            // This is thrown if even one of the files requires permissions greater
+            // than the application provides.
+            catch (UnauthorizedAccessException e)
+            {
+                // This code just writes out the message and continues to recurse.
+                // You may decide to do something different here. For example, you
+                // can try to elevate your privileges and access the file again.
+                log.Add(e.Message);
+            }
 
-        if (files != null)
-            //{
-            //    if (FolderMode == false)
-            //    {
-            //        foreach (System.IO.FileInfo fi in files)
-            //        {
-            //            if (fi.Name.Contains(Search))
-            //            {
-            //                Console.WriteLine("Found File");
-            //                Console.WriteLine(fi.FullName);
-            //                Console.WriteLine(fi.Directory);
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
 
-            //                Found_Install_Folder = true;
-            //                Found_Install = true; 
-            //                break;
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine("Trying again 2");
-            //                WalkDirectoryTree(root, Search, FolderMode);
+                if (files != null)
+                    //{
+                    //    if (FolderMode == false)
+                    //    {
+                    //        foreach (System.IO.FileInfo fi in files)
+                    //        {
+                    //            if (fi.Name.Contains(Search))
+                    //            {
+                    //                Console.WriteLine("Found File");
+                    //                Console.WriteLine(fi.FullName);
+                    //                Console.WriteLine(fi.Directory);
 
-            //            }
+                    //                Found_Install_Folder = true;
+                    //                Found_Install = true; 
+                    //                break;
+                    //            }
+                    //            else
+                    //            {
+                    //                Console.WriteLine("Trying again 2");
+                    //                WalkDirectoryTree(root, Search, FolderMode);
+
+                    //            }
 
 
-            //        }
-            //    }
-               
+                    //        }
+                    //    }
+
                     // Now find all the subdirectories under this directory.
                     subDirs = root.GetDirectories();
 
-            foreach (System.IO.DirectoryInfo dirInfo in subDirs)
-            {
-                if (dirInfo.Name.Contains(Search))
+
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
                 {
-                    Console.WriteLine("Found Folder");
-                    Console.WriteLine(dirInfo.FullName);
-                    Found_Install_Folder = true;
-                    Found_Install = true;
-                    break;
+                    if (dirInfo.Name.Contains(Search))
+                    {
+                        // Console.WriteLine("Found Folder");
+                        //  Console.WriteLine(dirInfo.FullName);
+                        Found_Install_Folder = true;
+                        Current_Install_Folder = (dirInfo.FullName);
+                        break;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Trying again at " + dirInfo);
+                      
+                      //  WalkDirectoryTree(dirInfo, Search);
+                    }
+                    if (dirInfo == null)
+                    {
+                        Console.WriteLine(dirInfo.FullName + "This is not a valid Folder????!");
+                        continue;
+
+                    }
+                    // Resursive call for each subdirectory.
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                log.Add(e.Message);
+
+
+            }
+
+        }
+
+        private void Install_NS_Button_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Browse_New_Install_Click(object sender, EventArgs e)
+        {
+            // Log_Box.AppendText(LookForTitanfallInstall());
+            while (Found_Install_Folder == false)
+            {
+                //    FindGitPath("dotnsset", ".exe", @"C:\Program Files");
+                Log_Box.AppendText("\nAutomatically Looking For The Northstar And Titandfall Install :-)");
+                Cursor.Current = Cursors.WaitCursor;
+
+                FindNSInstall("Titanfall2", @"C:\Program Files (x86)\Steam");
+                Log_Box.AppendText("Could not Find the Install at C:\\Program Files (x86)\\Steam - Continuing Traversal");
+                FindNSInstall("Titanfall2", @"D:\Games");
+                if (Found_Install_Folder == false)
+                {
+                    Log_Box.AppendText("\nCould Not Find, Please Manually Navigate");
+
+
+                }
+
+            }
+            if (Found_Install_Folder == true)
+            {
+                Install_Textbox.Text = Current_Install_Folder;
+                Log_Box.AppendText("\nFound Install Location at " + Current_Install_Folder + "\n");
+                NSExe = Get_And_Set_Filepaths(Current_Install_Folder, "NorthstarLauncher.exe");
+                //Checking if the path Given Returned Something Meaningful. I know i could do this better, but its 3.37am and i feel like im dying from this cold :|.
+                if (Directory.Exists(NSExe))
+                {
+                    NS_Installed = true;
 
                 }
                 else
                 {
-                    Console.WriteLine("Trying again");
-                    WalkDirectoryTree(dirInfo, Search);
 
-
+                    NS_Installed = false;
                 }
-                // Resursive call for each subdirectory.
-            }
-                
-        }
-    
-        private void Browse_New_Install_Click(object sender, EventArgs e)
-        {
-            // Log_Box.AppendText(LookForTitanfallInstall());
-            while (Found_Install == false || Found_Install_Folder == false)
-            {
-                //    FindGitPath("dotnsset", ".exe", @"C:\Program Files");
 
-                FindNSInstall("LycheeSlicer");
+                if (NS_Installed == false)
+                {
 
+                    Log_Box.AppendText("OH MY!, NorthStar Launcher Was not found, do you want to Install it by Clicking Install Northstar Launcher?");
+                    Version_TextBox.BackColor = Color.Red;
+                }
+
+                Gamever= Get_And_Set_Filepaths(Current_Install_Folder, "gameversion.txt");
+                Version_TextBox.Text = NSExe;
             }
+
         }
     }
 }
