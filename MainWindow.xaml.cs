49ee14e81de79063051af71badc1bdbb3f2be081
Chan-yeong Kang <rrrfffrrr@naver.com>
2022-05-05 21:08:25 +0900

    Prevent crash when press cancel on import server config dialog
    But we need to add zip file validation to prevent crash when wrong zip file provided.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7373,12 +7373,14 @@ Every cent counts towards feeding my baby Ticks - https://www.buymeacoffee.com/J
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Zip files (*.zip)|*.zip|All files (*.*)|*.*";
             openFileDialog.RestoreDirectory = true;
+
             if (openFileDialog.ShowDialog() == true)
             {
                 zipPath = openFileDialog.FileName;
-
-
+            } else {
+                return;
             }
+
             if (Directory.Exists(Current_Install_Folder + @"\VTOL_Dedicated_Workspace"))
             {
                 string extractPath = Path.GetFullPath(Current_Install_Folder + @"\VTOL_Dedicated_Workspace");
