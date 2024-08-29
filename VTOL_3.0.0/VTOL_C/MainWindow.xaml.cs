95306cb70287f81a37e96a65f03da2ff33f2589e
Jordan <jordankmuguluma@gmail.com>
2024-08-29 18:26:10 -0400

    It Lives!

--- VTOL_3.0.0/VTOL_C/MainWindow.xaml.cs
+++ VTOL_3.0.0/VTOL_C/MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Page = iNKORE.UI.WPF.Modern.Controls.Page;
+using iNKORE.UI.WPF.Modern.Controls;
+using VTOL_C.Pages;
+using iNKORE.UI.WPF.Modern.Media.Animation;
 
 namespace VTOL_C
 {
@@ -18,7 +22,42 @@ namespace VTOL_C
     {
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+        public Pages.Home_landing Page_Home = new Pages.Home_landing();
+        public Pages.Modifications Page_Modifications = new Pages.Modifications();
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Nav_Main_SelectionChanged(iNKORE.UI.WPF.Modern.Controls.NavigationView sender, iNKORE.UI.WPF.Modern.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = sender.SelectedItem;
+            Page? page = null;
+            if (item == Navitem_Home)
+            {
+                page = Page_Home;
+            }
+            else if (item == Navitem_Mods)
+            {
+                page = Page_Modifications;
+            }
+           
+
+            if (page != null)
+            {
+                Nav_Main.Header = page.Title;
+                Frame_Main.Navigate(page, new DrillInNavigationTransitionInfo());
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Nav_Main.SelectedItem = Navitem_Home;
+
         }
     }
 }
